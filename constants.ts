2d7db07dfbe0fc26ba8be3226070515b559cae31
srodriguev <srodriguev@eafit.edu.co>
2024-04-24 21:13:31 -0500

    env variables

--- constants.ts
+++ constants.ts
@@ -1,4 +1,6 @@
+const { ENDPOINT_API, STRIPE_KEY } = process.env;
+
 export default {
-    endpoint: 'http://localhost:9000/api/checkout',
-    stripe_key: 'pk_test_51P3dvERvKlWyhRDtF21EIXcMg74rqqqkQQMdOrymUpZFyyuJyVnHVe5sS23KJ0w8z6S3tgCodEtmji8DNp7goMo300f4fTnrxC'
-}
+    endpoint: `http://${ENDPOINT_API}`,
+    stripe_key: STRIPE_KEY
+};
